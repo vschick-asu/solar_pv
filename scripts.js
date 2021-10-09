adb7c895f6b69f04f2cc4081798c054871c9e218
Vladimir Schick <vschick@asu.edu>
2021-10-08 17:17:35 -0700

    copied everything from pd1 for the html portion

---
+++ scripts.js
@@ -0,0 +1,31 @@
+//drag and drop stuff
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+//geolocation stuff
+    function showPosition() {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
+                document.getElementById("result").innerHTML = positionInfo;
+            });
+        } else {
+            alert("Sorry, your browser does not support HTML5 geolocation.");
+        }
+    }
+
+//popup stuff
+function togglePopup() {
+    $(".content").toggle();
+}
