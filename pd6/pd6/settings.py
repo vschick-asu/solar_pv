b5a73362078f2097278274f4413db3479260ab04
Vladimir Schick <vschick@asu.edu>
2021-10-11 16:27:35 -0700

    I think this template is finally working to connect to my existing website

--- pd6/pd6/settings.py
+++ pd6/pd6/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'pd6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['pd6/template/solarpv/html_files/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pd6/solarpv/views.py
+++ pd6/solarpv/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    #return HttpResponse("Hello")
     return render(request, "index.html")
+def dashboard(request):
+    return render(request, "dashboard.html")
