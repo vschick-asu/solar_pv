d0e418fbd9ced965546330a6f698fe900c234c9b
Vladimir Schick <vschick@asu.edu>
2021-10-25 20:12:52 -0700

    added backend app

--- pd6/pd6/settings.py
+++ pd6/pd6/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'pd6.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'solarpv',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'PORT': '3306',
     }
 }
 
