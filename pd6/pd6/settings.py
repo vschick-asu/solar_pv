de9d7485251b5639c6fb91129186a0432ed2cd63
Vladimir Schick <schick268@gmail.com>
2021-11-21 18:59:22 -0800

    updated debug

--- pd6/pd6/settings.py
+++ pd6/pd6/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pyjmv-7e50&_x_*q)i3e3vqfhc2^*utzx$^w=l#3-o&qw%_ei='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["solarpv.tarxzvf.com"]
 
 
 # Application definition
